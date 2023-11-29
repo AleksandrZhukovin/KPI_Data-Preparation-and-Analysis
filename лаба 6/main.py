ab8dca18ebdd6cb4f59a96501c0cbbe096fe9d4f
AleksandrZhukovin <sashokzuk@gmail.com>
2023-11-29 15:38:31 +0200

    fix(laba 6)

--- лаба 6/main.py
+++ лаба 6/main.py
@@ -1,33 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+n = 100
 k = 2
 b = 3
-kf = np.random.random(1)[0]
-bf = np.random.random(1)[0]
-x = np.linspace(0, 20, 20)
+x = np.linspace(0, n, n)
 y = x * k + b
 vals = []
-
 for i in y:
     vals.append(np.random.normal(i, 2, 1)[0])
 vals = np.array(vals)
 
 
 def less_squares_method():
-    global bf, kf
+    kf = np.random.random(1)[0]
+    bf = np.random.random(1)[0]
     mean_x = np.mean(x)
     mean_y = np.mean(vals)
     kf = np.sum((x - mean_x) * (vals - mean_y)) / np.sum((x - mean_x) ** 2)
     bf = mean_y - kf * mean_x
+    return bf, kf
 
 
-def gradient_decent(n_iter, lr):
-    global bf, kf
-    for _ in range(n_iter):
+def gradient_decent(n_iter, lr, x, y):
+    errors = np.zeros(n_iter)
+    kf = np.random.random(1)[0]
+    bf = np.random.random(1)[0]
+    for i in range(n_iter):
         y1 = kf * x + bf
         kf += lr * (2 * np.sum(x * (vals - y1)))
         bf += lr * (2 * np.sum(vals - y1))
+        errors[i] = np.mean((y - y1) ** 2)
+    return bf, kf, errors
 
 
 def error(n_iter, lr):
@@ -40,25 +44,26 @@ def error(n_iter, lr):
     return np.sum((vals - (kt * x + bt))**2)
 
 
-less_squares_method()
+bf, kf = less_squares_method()
 new_y = x * kf + bf
 print(f"""Оцінка поліному степеню 1 для знайдених параметрів: {np.polyfit([bf], [kf], 1)},
 для початкових {np.polyfit([b], [k], 1)}""")
 
 kf = np.random.random(1)[0]
 bf = np.random.random(1)[0]
-gradient_decent(3, 0.0001)
-new_y1 = x * kf + bf
+bf1, kf1, errors = gradient_decent(1000, 0.001, x, y)
+print(errors)
+new_y1 = x * kf1 + bf1
 
 plt.subplot(1, 2, 1)
 plt.plot(x, new_y, label='Calculated line')
 plt.plot(x, new_y1, label='Calculated line (Grad. de.)')
 plt.plot(x, y, label='Start line')
-plt.scatter(x, vals)
+# plt.scatter(x, vals)
 plt.legend()
 
 plt.subplot(1, 2, 2)
 iters = [i for i in range(1, 11)]
-errors = [error(e, 0.0001) for e in range(1, 11)]
-plt.plot(iters, errors, label='Error function result')
+# errors = [error(e, 0.0001) for e in range(1, 11)]
+plt.plot(errors, label='Error function result')
 plt.show()
