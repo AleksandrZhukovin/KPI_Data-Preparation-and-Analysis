fix(lab5,6): fix noise work in lab5. fix gradient descent in lab 6

лаба 5/main.py
@@ -5,9 +5,12 @@ import scipy.signal as sc
 
 
 """Create plots"""
+noise_loc = 0
+noise = np.random.normal(0, noise_loc, 1000)
 t = np.linspace(0, 1, 1000)
 fig, (ax1, ax2) = plt.subplots(1, 2)
 line, = ax1.plot(t, 5 * np.sin(5 * t + 1), lw=2)
+line1, = ax1.plot(t, 5 * np.sin(5 * t + 1), lw=2, visible=False, alpha=0.5)
 filter_line, = ax2.plot(t, 5 * np.sin(5 * t + 1), lw=2)
 fig.subplots_adjust(left=0.15, bottom=0.3)
 
@@ -94,9 +97,12 @@ def harmonic_with_noise(amplitude, frequency, phase=1, noise_mean=0, show_noise=
     b, a = sc.iirfilter(int(filter_order), Wn=Wn, fs=fs, btype="low", ftype="butter")
     filtered_data = sc.lfilter(b, a, amplitude * np.sin(frequency * t + phase) + noise_mean)
     if show_noise:
-        line.set_ydata(amplitude * np.sin(frequency * t + phase) + noise_mean)
+        line.set_ydata(amplitude * np.sin(frequency * t + phase))
+        line1.set_ydata(amplitude * np.sin(frequency * t + phase) + noise_mean)
+        line1.set_visible(True)
     else:
         line.set_ydata(amplitude * np.sin(frequency * t + phase))
+        line1.set_visible(False)
     if own_filter:
         filter_line.set_ydata(my_own_filter(amplitude * np.sin(frequency * t + phase) + noise_mean))
     else:
@@ -105,19 +111,22 @@ def harmonic_with_noise(amplitude, frequency, phase=1, noise_mean=0, show_noise=
 
 
 def my_own_filter(data, buffer_size=7):
-    res = [0] * len(data)
+    res = np.zeros(len(data))
     for n in range(len(data)-buffer_size):
-        for i in range(buffer_size):
-            res[n] += data[n+i]
-        res[n] /= buffer_size
-
+        res[n] += sum(data[n:n+7])
+    res /= buffer_size
     return res
 
 
 """Plot updating"""
 def update(val):
+    global noise_loc
+    global noise
+    if noise_loc != noise_slider.val:
+        noise_loc = noise_slider.val
+        noise = np.random.normal(0, noise_loc, 1000)
     Wn_slider.valmax = sampling_rate_slider.val / 2 - 0.01
-    harmonic_with_noise(amp_slider.val, freq_slider.val, noise_mean=np.random.normal(0, noise_slider.val, 1000),
+    harmonic_with_noise(amp_slider.val, freq_slider.val, noise_mean=noise,
                         show_noise=check.get_status()[0], filter_order=filter_order_slider.val, Wn=Wn_slider.val,
                         fs=sampling_rate_slider.val, own_filter=use_own.get_status()[0])
 

лаба 6/main.py
@@ -2,19 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 n = 100
+y_size = 35
 k = 2
 b = 3
 x = np.linspace(0, n, n)
 y = x * k + b
-vals = []
-for i in y:
-    vals.append(np.random.normal(i, 2, 1)[0])
-vals = np.array(vals)
+vals = y + np.random.normal(0, y_size * 0.2, y.shape[0])
 
 
 def less_squares_method():
-    kf = np.random.random(1)[0]
-    bf = np.random.random(1)[0]
     mean_x = np.mean(x)
     mean_y = np.mean(vals)
     kf = np.sum((x - mean_x) * (vals - mean_y)) / np.sum((x - mean_x) ** 2)
@@ -22,48 +18,37 @@ def less_squares_method():
     return bf, kf
 
 
-def gradient_decent(n_iter, lr, x, y):
+def gradient_decent(n_iter, lr, X, Y):
     errors = np.zeros(n_iter)
     kf = np.random.random(1)[0]
     bf = np.random.random(1)[0]
     for i in range(n_iter):
-        y1 = kf * x + bf
-        kf += lr * (2 * np.sum(x * (vals - y1)))
-        bf += lr * (2 * np.sum(vals - y1))
-        errors[i] = np.mean((y - y1) ** 2)
+        y1 = kf * X + bf
+        kf += lr * (2 * np.mean(X * (vals - y1)))
+        bf += lr * (2 * np.mean(vals - y1))
+        errors[i] = np.mean((Y - y1) ** 2)
     return bf, kf, errors
 
 
-def error(n_iter, lr):
-    kt = np.random.random(1)[0]
-    bt = np.random.random(1)[0]
-    for _ in range(n_iter):
-        y1 = kt * x + bt
-        kt += lr * (2 * np.sum(x * (vals - y1)))
-        bt += lr * (2 * np.sum(vals - y1))
-    return np.sum((vals - (kt * x + bt))**2)
+b_lin, k_lin = less_squares_method()
+new_y = x * k_lin + b_lin
 
-
-bf, kf = less_squares_method()
-new_y = x * kf + bf
-print(f"""Оцінка поліному степеню 1 для знайдених параметрів: {np.polyfit([bf], [kf], 1)},
+print(f"""Оцінка поліному степеню 1 для знайдених параметрів: {np.polyfit([b_lin], [k_lin], 1)},
 для початкових {np.polyfit([b], [k], 1)}""")
 
-kf = np.random.random(1)[0]
-bf = np.random.random(1)[0]
-bf1, kf1, errors = gradient_decent(1000, 0.001, x, y)
-print(errors)
-new_y1 = x * kf1 + bf1
+
+b_grad, k_grad, errors_grad = gradient_decent(100, 0.0001, x, y)
+
+new_y1 = x * k_grad + b_grad
 
 plt.subplot(1, 2, 1)
 plt.plot(x, new_y, label='Calculated line')
 plt.plot(x, new_y1, label='Calculated line (Grad. de.)')
 plt.plot(x, y, label='Start line')
-# plt.scatter(x, vals)
+plt.scatter(x, vals)
 plt.legend()
 
 plt.subplot(1, 2, 2)
-iters = [i for i in range(1, 11)]
-# errors = [error(e, 0.0001) for e in range(1, 11)]
-plt.plot(errors, label='Error function result')
+
+plt.plot(errors_grad, label='Error function result')
 plt.show()